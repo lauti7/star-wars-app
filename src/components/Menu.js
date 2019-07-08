61d652727d801b34c56f2bca83954a99f5e4a6b2
Lautaro Bustos <lautarobustos@MacBook-Air-de-Lautaro.local>
2019-07-08 15:16:29 -0300

    created the nav component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,16 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Menu = () => {
   return(
-    <header className="sticky-top">
+    <header className="sticky-top mb-3">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <p className="navbar-brand" >Star Wars</p>
+          <Link to="/" className="navbar-brand" >Star Wars</Link>
           <ul className="navbar-nav">
             <li className="nav-item">
+              <Link className="nav-link" to="/personajes">Personajes</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/personajes">personajes</Link>
-            </li>
-            <li className="nav-item">
+              <Link className="nav-link" to="/favoritos">Favoritos</Link>
             </li>
           </ul>
         </nav>
